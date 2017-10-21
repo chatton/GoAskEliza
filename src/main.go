f43ffdeff8e6008d11b6486380f0f6bcf8fed242
chatton <cianhatton@gmail.com>
2017-10-21 13:04:48 +0100

    added /ask endpoint

--- src/main.go
+++ src/main.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 )
 
+
+var e *eliza.Eliza
+
 func main() {
 	var g eliza.AnswerGenerator // interface
 	// implementation
@@ -16,12 +19,32 @@ func main() {
 	var p eliza.AnswerPicker
 	p = pickers.NewRandomPicker()
 
-	e := eliza.NewEliza(g, p)
+	e = eliza.NewEliza(g, p)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		// serves up index.html when a request is made to "/"
 		http.ServeFile(w, r, "./html/index.html")
 	})
 
+	// when a request is made to /ask, the askEliza function will execute.
+	http.HandleFunc("/ask", askEliza);
+
 	http.ListenAndServe(":9999", nil);
-	fmt.Println(e.GoAsk("I like waffles %s!"))
+}
+
+func hasQuesion(r *http.Request) bool {
+	return r.URL.Query().Get("question") != ""
+}
+
+func getQuestion(r *http.Request) string {
+	return r.URL.Query().Get("question")
+}
+
+func askEliza(w http.ResponseWriter, r *http.Request) {
+	var question string
+	if hasQuesion(r) {
+		question = getQuestion(r)
+		response := e.GoAsk(question)
+		fmt.Println(response)
+	}
 }
