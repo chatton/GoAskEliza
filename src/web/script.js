48574e513bf9f16eb2e84be6ea4893a822b8e820
chatton <cianhatton@gmail.com>
2017-11-08 16:57:09 +0000

    switched button to Enter - suppressing default behaviour

--- src/web/script.js
+++ src/web/script.js
@@ -1,10 +1,20 @@
 var list = document.getElementById("conversation");
 var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function(){    
+keyCodes = {
+    ENTER : 13
+}
+
+document.addEventListener("keypress", function(e){
+    if(e.keyCode != keyCodes.ENTER){
+        return; // we want to ignore all keypresses other than enter.
+    }
+
     const userInput = document.getElementById("user-input");
     const question = userInput.value;
-    document.getElementById("user-input").value = "";
+
+    document.getElementById("user-input").value = ""; // wipe the text box clean.
+
     const request = new XMLHttpRequest();
     const params = "question=" + question;
     request.open("POST", "http://localhost:8080/ask", true);
@@ -15,10 +25,19 @@ btn.addEventListener("click", function(){
             addListItem("user_message", question);
             setTimeout(function(){
                 addListItem("eliza_message", response);
-            }, 2000); // add a 2 second delay to make it look like Eliza is "typing" her message. instead of instantly displaying it.
+            }, 1500); // add a delay to make it look like Eliza is "typing" her message. instead of instantly displaying it.
         }
     }
     request.send(params);
+});
+
+// found method to supress the default behaviour of the enter key here.
+// https://stackoverflow.com/questions/11235622/jquery-disable-form-submit-on-enter
+$('#user-input').on('keyup keypress', function(e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode === keyCodes.ENTER) { 
+        e.preventDefault(); // default behaviour is refreshing the page, which will reset the list and lose the converstaion.
+    }
 });
 
 function addListItem(speaker, text){
